8dd89fb8d43d3cdfeaaac46865658d47550e3576
Kelley Chaplain <kmchaplain@gmail.com>
2020-03-28 16:12:07 -0500

    fixed links on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     <footer className="page-footer font-small mdb-color darken-3 pt-4">
       <div className="d-flex flex-row justify-content-around">
         <p><span role="img" aria-label="yellow heart">&#x1F49B;</span> Made for the Rebel Forces by:
-        <a href="https://www.kelleymichelle.com/" target="_blank" rel="noopener noreferrer">  Kelley Chaplain </a>
+        <a href="http://www.kelleymichelle.com/" target="_blank" rel="noopener noreferrer">  Kelley Chaplain </a>
         &
-        <a href="https://www.jamievullo.com/" target="_blank" rel="noopener noreferrer">  Jamie Vullo </a>
+        <a href="http://www.jamievullo.com/" target="_blank" rel="noopener noreferrer">  Jamie Vullo </a>
         </p>
 
         <a href="https://www.matchymatch.net/" target="_blank" rel="noopener noreferrer"><span role="img" aria-label="heart eyes cat">&#x1F63B;</span> Click here to play Matchy-Match</a>
