6d4d0ef1b859b42a89fdf75ed5aaecd9a70edee8
Kelley Chaplain <kmchaplain@gmail.com>
2020-03-28 17:03:49 -0500

    worked on mobile responsiveness

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,15 +9,18 @@ if(!video) {
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
 
     return (
+        <>
         <div class="embed-responsive embed-responsive-21by9" style={{marginLeft: '2em'}}>
-            <div className="ui embed" style={{padding: '5%'}}>
-                <iframe style={{maxWidth: '640px', maxHeight: '360px', margin: '0px'}} class="embed-responsive-item" title="videoplayer" src={videoSrc} gesture="media" allow="encrypted-media" allowfullscreen/>
+            <div style={{margin: '5%'}}>
+                <iframe style={{maxWidth: '640px', maxHeight: '360px', margin: '0px'}} className="embed-responsive-item" title="videoplayer" src={videoSrc} gesture="media" allow="encrypted-media" allowfullscreen/>
             </div>
-            <div className="ui segment video-details">
-                <h4 className="ui header">{video.snippet.title}</h4>
-                <p>{video.snippet.description}</p>
-            </div>
+            
         </div>
+        <div className="video-details" style={{marginTop: '5%', marginLeft: '2em'}}>
+            <h5>{video.snippet.title}</h5>
+            <p>{video.snippet.description}</p>
+        </div>
+    </>
     )
 }
 
