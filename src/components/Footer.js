a63098374e8a82d85a3fca36278246bda8f787a3
Kelley Chaplain <kmchaplain@gmail.com>
2020-03-28 15:21:06 -0500

    added personal sites to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,12 @@ const Footer = () => {
   return (
     <footer className="page-footer font-small mdb-color darken-3 pt-4">
       <div className="d-flex flex-row justify-content-around">
-        <p><span role="img" aria-label="yellow heart">&#x1F49B;</span> Made for the Rebel Forces</p>
-        {/* <p>"Power! Unlimited Power!" Darth Sidious</p> */}
+        <p><span role="img" aria-label="yellow heart">&#x1F49B;</span> Made for the Rebel Forces by:
+        <a href="https://www.kelleymichelle.com/" target="_blank" rel="noopener noreferrer">  Kelley Michelle </a>
+        &
+        <a href="https://www.jamievullo.com/" target="_blank" rel="noopener noreferrer">  Jamie Vullo </a>
+        </p>
+
         <a href="https://www.matchymatch.net/" target="_blank" rel="noopener noreferrer"><span role="img" aria-label="heart eyes cat">&#x1F63B;</span> Click here to play Matchy-Match</a>
         <a href="https://projectmoonlight.herokuapp.com/" target="_blank" rel="noopener noreferrer"><span role="img" aria-label="moon">&#x1F314;</span> Click here to visit Project Moonlight</a>
       </div>
